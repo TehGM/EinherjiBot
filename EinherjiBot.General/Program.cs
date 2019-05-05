7bf70063552102539c97de281f6141a44c988b21
TehGM <wojop95@gmail.com>
2019-05-05 14:11:23 +0100

    Add project files.

---
+++ EinherjiBot.General/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EinherjiBot.General
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EinherjiBot.Pi120/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EinherjiBot.Pi120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
