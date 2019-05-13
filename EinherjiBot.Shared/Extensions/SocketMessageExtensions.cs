459286db08f94d675244196b0164528abebb04b3
TehGM <wojop95@gmail.com>
2019-05-13 13:25:49 +0100

    added return value

--- EinherjiBot.Shared/Extensions/SocketMessageExtensions.cs
+++ EinherjiBot.Shared/Extensions/SocketMessageExtensions.cs
@@ -5,15 +5,16 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Discord.Rest;
 
 namespace TehGM.EinherjiBot.Extensions
 {
     public static class SocketMessageExtensions
     {
-        public static Task ReplyAsync(this SocketCommandContext message, string text = null, bool isTTS = false, Embed embed = null, RequestOptions options = null)
+        public static Task<RestUserMessage> ReplyAsync(this SocketCommandContext message, string text = null, bool isTTS = false, Embed embed = null, RequestOptions options = null)
             => ReplyAsync(message.Message, text, isTTS, embed, options);
 
-        public static Task ReplyAsync(this SocketUserMessage message, string text = null, bool isTTS = false, Embed embed = null, RequestOptions options = null)
+        public static Task<RestUserMessage> ReplyAsync(this SocketUserMessage message, string text = null, bool isTTS = false, Embed embed = null, RequestOptions options = null)
             => message.Channel.SendMessageAsync(text, isTTS, embed, options);
     }
 }
