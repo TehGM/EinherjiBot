b24dd2b000af7e32468a09455c791b9a0cab6e95
TehGM <wojop95@gmail.com>
2019-05-16 15:25:31 +0200

    build info gathering command

---
+++ EinherjiBot.General/IntelHandler.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using TehGM.EinherjiBot.CommandsProcessing;
+using TehGM.EinherjiBot.Config;
+using TehGM.EinherjiBot.Extensions;
+
+namespace TehGM.EinherjiBot
+{
+    [ProductionOnly]
+    class IntelHandler : HandlerBase
+    {
+        public IntelHandler(DiscordSocketClient client, BotConfig config) : base(client, config)
+        {
+            CommandsStack.Add(new RegexUserCommand("^intel on me", CmdIntelMe));
+            CommandsStack.Add(new RegexUserCommand("^intel on \\\\?<@!?(\\d+)>", CmdIntelUser));
+            CommandsStack.Add(new RegexUserCommand("^intel on guild", CmdIntelGuild));
+            CommandsStack.Add(new RegexUserCommand("^intel", CmdIntelHelp));
+        }
+
+        private Task CmdIntelMe(SocketCommandContext message, Match match)
+            => ProcessIntelUser(message, message.User);
+
+        private Task CmdIntelUser(SocketCommandContext message, Match match)
+        {
+            string idString = match.Groups[1].Value;
+            if (!ulong.TryParse(idString, out ulong id))
+                return message.ReplyAsync($"Could not parse user ID `{idString}`.");
+            SocketUser user = Client.GetUser(id);
+            if (user == null)
+                return message.ReplyAsync($"Could not find user with ID `{id}`.");
+            return ProcessIntelUser(message, user);
+        }
+
+        private Task CmdIntelGuild(SocketCommandContext message, Match match)
+        {
+            if (message.IsPrivate)
+                return message.ReplyAsync("This command can only be used in a guild channel.");
+
+            EmbedBuilder embed = new EmbedBuilder();
+            AddGuildInfo(embed, message.Guild);
+            return message.ReplyAsync(null, false, embed.Build());
+        }
+
+        private Task CmdIntelHelp(SocketCommandContext message, Match match)
+        {
+            string prefix = (CommandVerificator.DefaultPrefixed as CommandVerificator).StringPrefix;
+            EmbedBuilder embed = new EmbedBuilder()
+                .AddField("Intel Commands",
+                    $"**{prefix}intel on me** - get intel on yourself\n" +
+                    $"**{prefix}intel on** ***<user ping>*** - get intel on pinged user\n" +
+                    $"**{prefix}intel on guild** - *(guild only)* get intel on current guild");
+            return message.ReplyAsync(null, false, embed.Build());
+        }
+
+        private Task ProcessIntelUser(SocketCommandContext message, IUser user)
+        {
+            EmbedBuilder embed = new EmbedBuilder();
+            AddUserInfo(embed, user);
+            if (!message.IsPrivate)
+            {
+                SocketGuildUser guildUser = message.Guild.GetUser(user.Id);
+                if (guildUser != null)
+                    AddGuildUserInfo(embed, guildUser);
+            }
+            return message.ReplyAsync(null, false, embed.Build());
+        }
+
+        private static EmbedBuilder AddUserInfo(EmbedBuilder embed, IUser user)
+        {
+            string activityString = user.Activity == null ? "-" : $"*{ActivityTypeToString(user.Activity.Type)}* `{user.Activity.Name}`";
+            embed.WithAuthor($"Intel on {user.Username}", user.GetAvatarUrl())
+                .WithThumbnailUrl(user.GetAvatarUrl())
+                .AddField("Username and Discriminator", $"{user.Username}#{user.Discriminator}")
+                .AddField("Account age", (DateTimeOffset.UtcNow - user.CreatedAt).ToLongFriendlyString())
+                .AddField("Status", user.Status.ToString(), true)
+                .AddField("Activity", activityString, true)
+                .AddField("User type", user.IsWebhook ? "Webhook" : user.IsBot ? "Bot" : "Normal user")
+                .WithTimestamp(user.CreatedAt)
+                .WithFooter($"User ID: {user.Id}", user.GetAvatarUrl());
+            return embed;
+        }
+
+        private static EmbedBuilder AddGuildUserInfo(EmbedBuilder embed, SocketGuildUser user)
+        {
+            IOrderedEnumerable<SocketRole> roles = user.Roles.Where(r => r.Id != user.Guild.EveryoneRole.Id).OrderByDescending(r => r.Position);
+            if (user.Nickname != null)
+                embed.AddField("Guild nickname", user.Nickname, true)
+                    .WithAuthor($"Intel on {user.Nickname}", user.GetAvatarUrl());
+            embed.AddField("Roles", string.Join(", ", roles.Select(r => MentionUtils.MentionRole(r.Id))), true);
+            if (user.JoinedAt != null)
+                embed.AddField("Time in this guild", (DateTimeOffset.UtcNow - user.JoinedAt.Value).ToLongFriendlyString(), true);
+            embed.Color = roles.FirstOrDefault(r => r.Color != Color.Default)?.Color;
+            return embed;
+        }
+
+        private static EmbedBuilder AddGuildInfo(EmbedBuilder embed, SocketGuild guild)
+        {
+            embed.WithAuthor(guild.Name, guild.IconUrl)
+                .WithThumbnailUrl(guild.IconUrl)
+                .AddField("Owner", MentionUtils.MentionUser(guild.OwnerId))
+                .AddField("Guild age", (DateTimeOffset.UtcNow - guild.CreatedAt).ToLongFriendlyString(), true)
+                .AddField("Members", guild.MemberCount.ToString(), true)
+                .AddField("Roles", guild.Roles.Count.ToString(), true)
+                .AddField("Channels", guild.Channels.Count.ToString(), true)
+                .AddField("Default channel", guild.DefaultChannel.Mention, true);
+            if (guild.AFKChannel != null)
+            {
+                embed.AddField("AFK Channel", MentionUtils.MentionChannel(guild.AFKChannel.Id), true)
+                    .AddField("AFK Timeout", TimeSpan.FromSeconds(guild.AFKTimeout).ToShortFriendlyString(), true);
+            }
+            embed.AddField("Custom emotes", $"{guild.Emotes.Count}: {string.Join(' ', guild.Emotes.Select(e => e.ToString()))}")
+                .AddField("Admin 2FA requirement", guild.MfaLevel.ToString(), true)
+                .AddField("Verification level", guild.VerificationLevel.ToString(), true)
+                .AddField("Default notifications", DefaultMessageNotificationsToString(guild.DefaultMessageNotifications), true)
+                .AddField("Explicit content filter", ExplicitContentFilterLevelToString(guild.ExplicitContentFilter), true)
+                .WithTimestamp(guild.CreatedAt)
+                .WithFooter($"Guild ID: {guild.Id}", guild.IconUrl);
+            return embed;
+        }
+
+        protected static string DefaultMessageNotificationsToString(DefaultMessageNotifications value)
+        {
+            switch (value)
+            {
+                case DefaultMessageNotifications.AllMessages:
+                    return "All messages";
+                case DefaultMessageNotifications.MentionsOnly:
+                    return "Mentions only";
+                default:
+                    return null;
+            }
+        }
+
+        protected static string ExplicitContentFilterLevelToString(ExplicitContentFilterLevel value)
+        {
+            switch (value)
+            {
+                case ExplicitContentFilterLevel.Disabled:
+                    return "Disabled";
+                case ExplicitContentFilterLevel.MembersWithoutRoles:
+                    return "Filtered for members without roles";
+                case ExplicitContentFilterLevel.AllMembers:
+                    return "Filtered for all members";
+                default:
+                    return null;
+            }
+        }
+
+        protected static string ActivityTypeToString(ActivityType value)
+        {
+            switch (value)
+            {
+                case ActivityType.Listening:
+                    return "Listening to";
+                case ActivityType.Playing:
+                    return "Playing";
+                case ActivityType.Streaming:
+                    return "Streaming";
+                case ActivityType.Watching:
+                    return "Watching";
+                default:
+                    return null;
+            }
+        }
+    }
+}
--- EinherjiBot.Shared/Extensions/TimeSpanExtensions.cs
+++ EinherjiBot.Shared/Extensions/TimeSpanExtensions.cs
@@ -8,5 +8,9 @@ namespace TehGM.EinherjiBot.Extensions
     {
         public static string ToFriendlyString(this TimeSpan span)
                => $"{(int)span.TotalHours} hours {(int)span.Minutes} minutes {(int)span.Seconds} seconds";
+        public static string ToLongFriendlyString(this TimeSpan span)
+               => $"{span.Days} days, {(int)span.Hours} hours, {(int)span.Minutes} minutes and {(int)span.Seconds} seconds";
+        public static string ToShortFriendlyString(this TimeSpan span)
+               => $"{(int)span.TotalMinutes} minutes and {(int)span.Seconds} seconds";
     }
 }
