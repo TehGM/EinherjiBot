57b730f61410571a1287ed26ad1dd2b4c5ae722d
TehGM <wojop95@gmail.com>
2019-05-19 21:25:55 +0200

    implemented ability to ban and kick

---
+++ EinherjiBot.Shared/Extensions/ClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TehGM.EinherjiBot.Extensions
+{
+    public static class ClientExtensions
+    {
+        public static async Task<IUser> GetUserAsync(this DiscordSocketClient client, ulong id, CacheMode mode = CacheMode.AllowDownload)
+        {
+            IUser user = client.GetUser(id);
+            if (user == null && mode == CacheMode.AllowDownload)
+                return await client.Rest.GetUserAsync(id);
+            return user;
+        }
+    }
+}
