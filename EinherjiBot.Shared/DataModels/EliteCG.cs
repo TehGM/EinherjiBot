73ef2a838aebe0db49e237da7bb05b9a91228639
TehGM <wojop95@gmail.com>
2019-06-07 12:05:09 +0200

    Added model for CG

---
+++ EinherjiBot.Shared/DataModels/EliteCG.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TehGM.EinherjiBot.DataModels
+{
+    public sealed class EliteCG : IEquatable<EliteCG>
+    {
+        [JsonProperty("communitygoalName")]
+        public string Name { get; private set; }
+        [JsonProperty("starsystemName")]
+        public string SystemName { get; private set; }
+        [JsonProperty("stationName")]
+        public string StationName { get; private set; }
+        [JsonProperty("goalExpiry")]
+        public DateTime ExpirationTimeUtc { get; private set; }
+        [JsonProperty("tierReached")]
+        public uint TierReached { get; private set; }
+        [JsonProperty("tierMax")]
+        public uint TierMax { get; private set; }
+        [JsonProperty("contributorsNum")]
+        public uint ContributingPilotsCount { get; private set; }
+        [JsonProperty("contributionsTotal")]
+        public uint ContributionsCount { get; private set; }
+        [JsonProperty("isCompleted")]
+        public bool IsCompleted { get; private set; }
+        [JsonProperty("lastUpdate")]
+        public DateTime LastUpdateTimeUtc { get; private set; }
+        [JsonProperty("goalObjectiveText")]
+        public string Objective { get; private set; }
+        [JsonProperty("goalRewardText")]
+        public string Reward { get; private set; }
+        [JsonProperty("goalDescriptionText")]
+        public string Description { get; private set; }
+        [JsonProperty("inaraURL")]
+        public string InaraURL { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is EliteCG cg)
+                return Equals(cg);
+            return false;
+        }
+
+        public bool Equals(EliteCG other)
+        {
+            return Name == other.Name &&
+                   ExpirationTimeUtc == other.ExpirationTimeUtc;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 420877175;
+            hashCode = hashCode * -1521134295 + Name.GetHashCode();
+            hashCode = hashCode * -1521134295 + ExpirationTimeUtc.GetHashCode();
+            return hashCode;
+        }
+    }
+}
