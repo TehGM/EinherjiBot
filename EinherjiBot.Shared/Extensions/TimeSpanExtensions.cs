4283334897eed5c9810159783f5dfc014ce815a2
TehGM <wojop95@gmail.com>
2019-06-13 20:20:58 +0200

    minor fixes

--- EinherjiBot.Shared/Extensions/TimeSpanExtensions.cs
+++ EinherjiBot.Shared/Extensions/TimeSpanExtensions.cs
@@ -7,7 +7,7 @@ namespace TehGM.EinherjiBot.Extensions
     public static class TimeSpanExtensions
     {
         public static string ToFriendlyString(this TimeSpan span)
-               => $"{(int)span.TotalHours} hours {(int)span.Minutes} minutes {(int)span.Seconds} seconds";
+               => $"{(int)span.TotalHours} hours, {(int)span.Minutes} minutes and {(int)span.Seconds} seconds";
         public static string ToLongFriendlyString(this TimeSpan span)
                => $"{span.Days} days, {(int)span.Hours} hours, {(int)span.Minutes} minutes and {(int)span.Seconds} seconds";
         public static string ToShortFriendlyString(this TimeSpan span)
