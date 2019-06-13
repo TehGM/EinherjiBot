6016fc129a7ed322795f5fda65f980c49089a83b
TehGM <wojop95@gmail.com>
2019-06-13 20:20:37 +0200

    added default value for normal separator

--- EinherjiBot.Shared/Extensions/EnumerableExtensions.cs
+++ EinherjiBot.Shared/Extensions/EnumerableExtensions.cs
@@ -7,7 +7,7 @@ namespace TehGM.EinherjiBot.Extensions
 {
     public static class EnumerableExtensions
     {
-        public static string JoinAsSentence<T>(this IEnumerable<T> enumerable, string normalSeparator, string lastSeparator = " and ")
+        public static string JoinAsSentence<T>(this IEnumerable<T> enumerable, string normalSeparator = ", ", string lastSeparator = " and ")
         {
             int count = enumerable?.Count() ?? default;
             if (count == default)
