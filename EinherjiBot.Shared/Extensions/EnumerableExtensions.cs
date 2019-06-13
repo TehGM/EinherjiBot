690dba1fd136cddbf485d4a37d792e996fc2733b
TehGM <wojop95@gmail.com>
2019-06-13 20:10:45 +0200

    Added extension for fancy joining string

---
+++ EinherjiBot.Shared/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TehGM.EinherjiBot.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static string JoinAsSentence<T>(this IEnumerable<T> enumerable, string normalSeparator, string lastSeparator = " and ")
+        {
+            int count = enumerable?.Count() ?? default;
+            if (count == default)
+                return null;
+            string lastValue = enumerable.Last().ToString();
+            if (count == 1)
+                return lastValue;
+            return $"{string.Join(normalSeparator, enumerable.Take(count - 1))}{lastSeparator}{lastValue}";
+        }
+    }
+}
