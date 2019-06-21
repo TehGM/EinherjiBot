b4071d5eff0b1747c2cda34cfa004976e3378dd1
TehGM <wojop95@gmail.com>
2019-06-21 18:35:19 +0200

    added data classes

---
+++ EinherjiBot.Shared/Config/PatchbotHelperData.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TehGM.EinherjiBot.DataModels;
+
+namespace TehGM.EinherjiBot.Config
+{
+    public class PatchbotHelperData
+    {
+        [JsonProperty("games", Required = Required.Always)]
+        public PatchbotHelperGame[] Games { get; private set; }
+
+        public PatchbotHelperGame FindGame(string nameOrAlias)
+        {
+            if (Games == null || Games.Length == 0)
+                return null;
+            for (int i = 0; i < Games.Length; i++)
+            {
+                PatchbotHelperGame game = Games[i];
+                if (string.Equals(game.Name, nameOrAlias, StringComparison.OrdinalIgnoreCase))
+                    return game;
+                if (game.Aliases == null)
+                    continue;
+                for (int ii = 0; i < game.Aliases.Length; ii++)
+                {
+                    if (string.Equals(game.Aliases[ii], nameOrAlias, StringComparison.OrdinalIgnoreCase))
+                        return game;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ EinherjiBot.Shared/DataModels/PatchbotHelperGame.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TehGM.EinherjiBot.DataModels
+{
+    public class PatchbotHelperGame
+    {
+        [JsonProperty("name", Required = Required.Always)]
+        public string Name { get; private set; }
+        [JsonProperty("aliases")]
+        public string[] Aliases { get; private set; }
+        [JsonProperty("subscribersIds")]
+        public List<ulong> SubscribersIDs { get; private set; }
+
+        public bool AddSubscriber(ulong id)
+        {
+            if (SubscribersIDs == null)
+                SubscribersIDs = new List<ulong>();
+            else if (SubscribersIDs.Contains(id))
+                return false;
+            SubscribersIDs.Add(id);
+            return true;
+        }
+
+        public bool RemoveSubscriber(ulong id)
+        {
+            if (SubscribersIDs == null)
+                return false;
+            return SubscribersIDs.Remove(id);
+        }
+    }
+}
