68e799d26e03df74fdc4c801ad2a53c999bb61f4
TehGM <wojop95@gmail.com>
2019-08-25 02:33:06 +0100

    Added base classes for permit functionality

---
+++ EinherjiBot.General/PermitsHandler.cs
@@ -0,0 +1,122 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TehGM.EinherjiBot.CommandsProcessing;
+using TehGM.EinherjiBot.Config;
+using TehGM.EinherjiBot.DataModels.Permits;
+using TehGM.EinherjiBot.Extensions;
+
+namespace TehGM.EinherjiBot
+{
+    class PermitsHandler : HandlerBase
+    {
+        public PermitsHandler(DiscordSocketClient client, BotConfig config) : base(client, config)
+        {
+            // netflix permit
+            CommandsStack.Add(new RegexUserCommand("^netflix-test set (login|email|username|password|pass|pwd) (.+)", (msg, match) => CmdSet(msg, match, config.Data.NetflixAccount)));
+            CommandsStack.Add(new RegexUserCommand("^netflix-test (?:password|account|login)", (msg, match) => CmdRetrieve(msg, match, config.Data.NetflixAccount)));
+        }
+
+        private async Task CmdRetrieve(SocketCommandContext message, Match match, PermitInfo permit)
+        {
+            if (message.IsPrivate)
+            {
+                await SendError($"{Config.DefaultReject} You can't do this in private message.\nGo to {GetAllowedChannelsMentionsText(permit)}.", message.Channel);
+                return;
+            }
+            SocketGuildUser user = await message.Guild.GetGuildUser(message.User);
+            if (!permit.CanRetrieve(user))
+            {
+                await SendError($"{Config.DefaultReject} You need {GetAllowedRolesMentionsText(permit)} role to do this.", message.Channel);
+                return;
+            }
+            if (!permit.IsChannelAllowed(message.Channel))
+            {
+                await SendError($"You can't do this here.\nGo to {GetAllowedChannelsMentionsText(permit)}.", message.Channel);
+                return;
+            }
+
+            // create message
+            IUser modifiedUser = Client.GetUser(permit.LastModifiedByID);
+            EmbedBuilder embed = permit.CreateConfirmationEmbed(modifiedUser);
+            // send message
+            RestUserMessage sentMsg = await message.ReplyAsync(GetWarningIfAutoremoving(permit, false), false, embed.Build());
+            // auto remove
+            if (permit.IsAutoRemoving)
+                await RemoveMessagesDelayed(permit.AutoRemoveDelay, sentMsg);
+        }
+
+        private async Task CmdSet(SocketCommandContext message, Match match, PermitInfo permit)
+        {
+            if (message.IsPrivate)
+            {
+                await SendError($"{Config.DefaultReject} You can't do this in private message.\nGo to {GetAllowedChannelsMentionsText(permit)}.", message.Channel);
+                return;
+            }
+            SocketGuildUser user = await message.Guild.GetGuildUser(message.User);
+            if (!permit.CanModify(user))
+            {
+                await SendError($"{Config.DefaultReject} You have no permissions to do this.", message.Channel);
+                return;
+            }
+            if (!permit.IsChannelAllowed(message.Channel))
+            {
+                await SendError($"You can't do this here.\nGo to {GetAllowedChannelsMentionsText(permit)}.", message.Channel);
+                return;
+            }
+
+            PermitInfo.UpdateResult result = permit.Update(message, match);
+            EmbedBuilder embed;
+            if (result.IsSuccess)
+            {
+                await Config.SaveAllAsync();
+                // create message
+                embed = permit.CreateConfirmationEmbed(message.User);
+                embed.WithDescription(result.Message);
+            }
+            else
+            {
+                embed = new EmbedBuilder()
+                    .WithDescription($"{Config.DefaultReject} {result.Message}")
+                    .WithColor(255, 0, 0);
+            }
+            //send message
+            RestUserMessage sentMsg = await message.ReplyAsync(GetWarningIfAutoremoving(permit), false, embed.Build());
+            // auto remove
+            if (permit.IsAutoRemoving)
+                await RemoveMessagesDelayed(permit.AutoRemoveDelay, sentMsg, message.Message);
+        }
+
+        private string GetAllowedRolesMentionsText(PermitInfo permit)
+            => permit.RetrieveRolesID.Select(id => MentionUtils.MentionRole(id)).JoinAsSentence(lastSeparator: " or ");
+
+        private string GetAllowedChannelsMentionsText(PermitInfo permit)
+            => permit.AllowedChannelsIDs.Select(id => MentionUtils.MentionChannel(id)).JoinAsSentence(lastSeparator: " or ");
+
+        private async Task RemoveMessagesDelayed(TimeSpan delay, params IMessage[] messages)
+        {
+            if (messages.Length == 0)
+                return;
+            SocketTextChannel channel = messages[0].Channel as SocketTextChannel;
+            await Task.Delay(delay);
+            await channel.DeleteMessagesAsync(messages);
+        }
+
+        private string GetWarningIfAutoremoving(PermitInfo permit, bool removingSendersMsg = true)
+            => permit.IsAutoRemoving ? $"I will remove this{(removingSendersMsg ? " and your" : null)} message in {permit.AutoRemoveDelay.ToShortFriendlyString()}." : null;
+
+        private Task SendError(string text, IMessageChannel channel, string mention = null)
+        {
+            EmbedBuilder embed = new EmbedBuilder()
+                .WithColor(255, 0, 0)
+                .WithTitle("Error")
+                .WithDescription(text);
+            return channel.SendMessageAsync(mention, false, embed.Build());
+        }
+    }
+}
---
+++ EinherjiBot.Shared/DataModels/Permits/PermitInfo.cs
@@ -0,0 +1,93 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TehGM.EinherjiBot.DataModels.Permits
+{
+    public abstract class PermitInfo
+    {
+        [JsonProperty("modifiedUserId")]
+        public ulong LastModifiedByID { get; private set; }
+        [JsonProperty("modifiedTime")]
+        public DateTimeOffset LastModifiedTimeUtc { get; private set; }
+        [JsonProperty("retrieveRolesIds", Required = Required.Always)]
+        public HashSet<ulong> RetrieveRolesID { get; set; }
+        [JsonProperty("modifyUsersIds", Required = Required.Default)]
+        public HashSet<ulong> ModifyUsersIDs { get; set; }
+        [JsonProperty("allowedChannelsIds", Required = Required.Always)]
+        public HashSet<ulong> AllowedChannelsIDs { get; set; }
+        [JsonProperty("autoremoveDelaySecs", DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+        [DefaultValue(5 * 60 * 1000)]
+        private int _autoremoveDelayMs;
+        [JsonIgnore]
+        public TimeSpan AutoRemoveDelay
+        {
+            get { return TimeSpan.FromSeconds(_autoremoveDelayMs); }
+            set { _autoremoveDelayMs = (int)value.TotalSeconds; }
+        }
+        [JsonIgnore]
+        public bool IsAutoRemoving => _autoremoveDelayMs > 0;
+
+        public bool CanRetrieve(SocketGuildUser user)
+            => RetrieveRolesID.Intersect(user.Roles.Select(r => r.Id)).Any();
+        public bool CanModify(IUser user)
+            => ModifyUsersIDs.Contains(user.Id);
+
+        public bool IsChannelAllowed(ulong channelID)
+            => AllowedChannelsIDs.Contains(channelID);
+        public bool IsChannelAllowed(IChannel channel)
+            => IsChannelAllowed(channel.Id);
+
+        public void UpdateLastModified(ulong userID)
+        {
+            if (userID == 0)
+                return;
+
+            this.LastModifiedByID = userID;
+            this.LastModifiedTimeUtc = DateTimeOffset.UtcNow;
+        }
+
+        public virtual EmbedBuilder CreateConfirmationEmbed(IUser lastModifiedUser)
+        {
+            EmbedBuilder embed = new EmbedBuilder();
+            if (this.LastModifiedByID != 0)
+            {
+                embed.WithTimestamp(this.LastModifiedTimeUtc)
+                .WithFooter($"Last modified by {lastModifiedUser.Username}#{lastModifiedUser.Discriminator}", lastModifiedUser.GetAvatarUrl());
+            }
+            embed.WithColor(0, 255, 0);
+            AddFieldsToEmbed(ref embed);
+            return embed;
+        }
+
+        protected abstract void AddFieldsToEmbed(ref EmbedBuilder embed);
+        protected abstract UpdateResult UpdateData(SocketCommandContext message, Match match);
+
+        public UpdateResult Update(SocketCommandContext message, Match match)
+        {
+            UpdateResult result = UpdateData(message, match);
+            if (result.IsSuccess)
+                UpdateLastModified(message.User.Id);
+            return result;
+        }
+
+
+        public class UpdateResult
+        {
+            public readonly string Message;
+            public readonly bool IsSuccess;
+
+            public UpdateResult(bool isSuccess, string message)
+            {
+                this.Message = message;
+                this.IsSuccess = isSuccess;
+            }
+        }
+    }
+}
