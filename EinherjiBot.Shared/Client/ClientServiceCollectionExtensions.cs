de567bf5f319f7eb65a7de3c62f56a8a04d43ae3
TehGM <wojop95@gmail.com>
2020-10-18 13:22:10 +0100

    Add custom hosted client

---
+++ EinherjiBot.Shared/Client/ClientServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TehGM.EinherjiBot.Client
+{
+    public static class ClientServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDiscordClient(this IServiceCollection services, Action<DiscordOptions> configure = null)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            if (configure != null)
+                services.Configure(configure);
+
+            services.AddSingleton<IHostedDiscordClient, HostedDiscordClient>();
+            services.AddTransient<IHostedService>(s => (IHostedService)s.GetRequiredService<IHostedDiscordClient>());
+            services.AddTransient<IDiscordClient>(s => s.GetRequiredService<IHostedDiscordClient>().Client);
+            services.AddTransient<DiscordSocketClient>(s => (DiscordSocketClient)s.GetRequiredService<IDiscordClient>());
+
+            return services;
+        }
+    }
+}
---
+++ EinherjiBot.Shared/Client/IHostedDiscordClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace TehGM.EinherjiBot
+{
+    public interface IHostedDiscordClient
+    {
+        IDiscordClient Client { get; }
+
+        Task StartClientAsync();
+        Task StopClientAsync();
+    }
+}
