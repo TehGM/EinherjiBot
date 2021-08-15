69c5c645cee8270c2d2873ce9d51f30b87c846a2
TehGM <wojop95@gmail.com>
2021-08-15 18:43:03 +0200

    Added functionality to deafen/undeafen users

--- EinherjiBot.General/Administration/VoiceChannelsCommandsHandler.cs
+++ EinherjiBot.General/Administration/VoiceChannelsCommandsHandler.cs
@@ -35,75 +35,102 @@ namespace TehGM.EinherjiBot.Administration
         [RegexCommand(@"^mute\s?all(?:(?: in)?\s+(?:<#)?(\d+)(?:>)?)?")]
         [Name("mute all [<channel-id>]")]
         [Summary("Mutes all users in a voice channel (either by ID, or the one you're currently in). You need to have appropiate permissions.")]
-        [Priority(-27)]
+        [Priority(-25)]
         private async Task CmdMuteAllAsync(SocketCommandContext context, Match match, CancellationToken cancellationToken = default)
         {
             using IDisposable logScope = _log.BeginCommandScope(context, this);
-            await ProcessMuteCommandAsync(context, match, true, cancellationToken).ConfigureAwait(false);
+
+            VoiceChannelMatch channelMatch = await this.MatchChannelAsync(context, match, 1, cancellationToken).ConfigureAwait(false);
+            VoiceChannelAction action = new VoiceChannelAction(user => user.Mute = true, ChannelPermission.DeafenMembers, "Muting", "muted", "mute");
+
+            await PerformActionOnAllAsync(context, channelMatch, action, cancellationToken).ConfigureAwait(false);
         }
 
         [RegexCommand(@"^unmute\s?all(?:(?: in)?\s+(?:<#)?(\d+)(?:>)?)?")]
         [Name("unmute all [<channel-id>]")]
         [Summary("Unmutes all users in a voice channel (either by ID, or the one you're currently in). You need to have appropiate permissions.")]
-        [Priority(-28)]
+        [Priority(-26)]
         private async Task CmdUnmuteAllAsync(SocketCommandContext context, Match match, CancellationToken cancellationToken = default)
         {
             using IDisposable logScope = _log.BeginCommandScope(context, this);
-            await ProcessMuteCommandAsync(context, match, false, cancellationToken).ConfigureAwait(false);
+
+            VoiceChannelMatch channelMatch = await this.MatchChannelAsync(context, match, 1, cancellationToken).ConfigureAwait(false);
+            VoiceChannelAction action = new VoiceChannelAction(user => user.Mute = false, ChannelPermission.DeafenMembers, "Unmuting", "unmuted", "unmute");
+
+            await PerformActionOnAllAsync(context, channelMatch, action, cancellationToken).ConfigureAwait(false);
         }
 
-        private async Task ProcessMuteCommandAsync(SocketCommandContext context, Match match, bool muting, CancellationToken cancellationToken)
+        [RegexCommand(@"^deafen\s?all(?:(?: in)?\s+(?:<#)?(\d+)(?:>)?)?")]
+        [Name("deafen all [<channel-id>]")]
+        [Summary("Deafens all users in a voice channel (either by ID, or the one you're currently in). You need to have appropiate permissions.")]
+        [Priority(-27)]
+        private async Task CmdDeafenAllAsync(SocketCommandContext context, Match match, CancellationToken cancellationToken = default)
+        {
+            using IDisposable logScope = _log.BeginCommandScope(context, this);
+
+            VoiceChannelMatch channelMatch = await this.MatchChannelAsync(context, match, 1, cancellationToken).ConfigureAwait(false);
+            VoiceChannelAction action = new VoiceChannelAction(user => user.Deaf = true, ChannelPermission.DeafenMembers, "Deafening", "deafened", "deafen");
+
+            await PerformActionOnAllAsync(context, channelMatch, action, cancellationToken).ConfigureAwait(false);
+        }
+
+        [RegexCommand(@"^undeafen\s?all(?:(?: in)?\s+(?:<#)?(\d+)(?:>)?)?")]
+        [Name("undeafen all [<channel-id>]")]
+        [Summary("Undeafens all users in a voice channel (either by ID, or the one you're currently in). You need to have appropiate permissions.")]
+        [Priority(-28)]
+        private async Task CmdUndeafenAllAsync(SocketCommandContext context, Match match, CancellationToken cancellationToken = default)
+        {
+            using IDisposable logScope = _log.BeginCommandScope(context, this);
+
+            VoiceChannelMatch channelMatch = await this.MatchChannelAsync(context, match, 1, cancellationToken).ConfigureAwait(false);
+            VoiceChannelAction action = new VoiceChannelAction(user => user.Deaf = false, ChannelPermission.DeafenMembers, "Undeafening", "undeafened", "undeafen");
+
+            await PerformActionOnAllAsync(context, channelMatch, action, cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task PerformActionOnAllAsync(SocketCommandContext context, VoiceChannelMatch channelMatch, VoiceChannelAction action, CancellationToken cancellationToken)
         {
             EinherjiOptions options = _einherjiOptions.CurrentValue;
 
+            if (!channelMatch.IsSuccess)
+                return;
+
             // verify it's a guild message
-            SocketTextChannel channel = await this.VerifyGuildChannelAsync(context, cancellationToken).ConfigureAwait(false);
-            if (channel == null)
+            SocketTextChannel responseChannel = await this.VerifyGuildChannelAsync(context, cancellationToken).ConfigureAwait(false);
+            if (responseChannel == null)
                 return;
 
-            // get voice channel ID
-            SocketVoiceChannel targetChannel = null;
-            SocketGuildUser user = context.Guild.GetUser(context.User.Id);
-            if (match.Groups.Count == 2)
-                targetChannel = await this.VerifyValidVoiceChannelAsync(match.Groups[1], context.Guild, context.Channel, cancellationToken).ConfigureAwait(false);
-            else
-                targetChannel = user.VoiceChannel;
-            if (targetChannel == null)
-                await context.ReplyAsync($"{options.FailureSymbol} You need to either provide a voice channel ID, or be in a voice channel currently. Also ensure I have access to that voice channel.", cancellationToken).ConfigureAwait(false);
-
-
-            _log.LogTrace("Verifying user {ID} has permission to mute users in channel {ChannelName} ({ChannelFromID})", user.Id, targetChannel.Name, targetChannel.Id);
-            if (!await this.VerifyUserCanConnectAsync(targetChannel, user, channel, cancellationToken).ConfigureAwait(false))
+            // verify permissions
+            _log.LogTrace("Verifying user {ID} has {Permission} permission in channel {ChannelName} ({ChannelFromID})", channelMatch.User.Id, action.RequiredPermission, channelMatch.Channel.Name, channelMatch.Channel.Id);
+            if (!await this.VerifyUserCanConnectAsync(channelMatch.Channel, channelMatch.User, responseChannel, cancellationToken).ConfigureAwait(false))
                 return;
-            if (!user.GuildPermissions.Administrator && !user.GetPermissions(targetChannel).MuteMembers)
+            if (!channelMatch.User.GuildPermissions.Administrator && !channelMatch.User.GetPermissions(channelMatch.Channel).Has(action.RequiredPermission))
             {
-                await channel.SendMessageAsync($"{options.FailureSymbol} You don't have *Mute Members* permission in {GetVoiceChannelMention(targetChannel)}.", cancellationToken).ConfigureAwait(false);
+                await responseChannel.SendMessageAsync($"{options.FailureSymbol} You don't have *{action.PermissionDisplayName}* permission in {GetVoiceChannelMention(channelMatch.Channel)}.", cancellationToken).ConfigureAwait(false);
                 return;
             }
 
 
-            // mute/unmute the users
-            SocketGuildUser[] users = targetChannel.Users.ToArray();
-            string channelMention = GetVoiceChannelMention(targetChannel);
-            string mode = muting ? "Muting" : "Unmuting";
-            _log.LogDebug($"{mode} {{Count}} users from channel {{ChannelName}} ({{ChannelID}})", users.Length, targetChannel.Name, targetChannel.Id);
-            RestUserMessage response = await context.ReplyAsync($"{mode} {users.Length} user{(users.Length > 1 ? "s" : null)} in {channelMention}.", cancellationToken).ConfigureAwait(false);
+            // perform the action on the users
+            SocketGuildUser[] users = channelMatch.Channel.Users.ToArray();
+            string channelMention = GetVoiceChannelMention(channelMatch.Channel);
+            _log.LogDebug($"{action.ModeWord} {{Count}} users from channel {{ChannelName}} ({{ChannelID}})", users.Length, channelMatch.Channel.Name, channelMatch.Channel.Id);
+            RestUserMessage response = await context.ReplyAsync($"{action.ModeWord} {users.Length} user{(users.Length > 1 ? "s" : null)} in {channelMention}.", cancellationToken).ConfigureAwait(false);
             int errCount = 0;
             for (int i = 0; i < users.Length; i++)
             {
                 try
                 {
-                    await users[i].ModifyAsync(props => props.Mute = muting, cancellationToken).ConfigureAwait(false);
+                    await users[i].ModifyAsync(action.Action, cancellationToken).ConfigureAwait(false);
                 }
                 catch { errCount++; }
             }
             // display confirmation
             StringBuilder builder = new StringBuilder();
             int successCount = users.Length - errCount;
-            string modePrefix = muting ? null : "un";
-            builder.AppendFormat("{0} user{1} {2}muted in {3}.", successCount.ToString(), successCount > 1 || successCount == 0 ? "s" : null, modePrefix, channelMention);
+            builder.AppendFormat("{0} user{1} {2} in {3}.", successCount.ToString(), successCount > 1 || successCount == 0 ? "s" : null, action.ActionCompletedWord, channelMention);
             if (errCount > 0)
-                builder.AppendFormat("\nFailed to {3}mute {0} user{2}. {1} Please make sure I have correct permissions!", errCount.ToString(), options.FailureSymbol, errCount > 1 ? "s" : null, modePrefix);
+                builder.AppendFormat("\nFailed to {3} {0} user{2}. {1} Please make sure I have correct permissions!", errCount.ToString(), options.FailureSymbol, errCount > 1 ? "s" : null, action.ActionFailedWord);
             await response.ModifyAsync(props => props.Content = builder.ToString(), cancellationToken).ConfigureAwait(false);
         }
 
@@ -183,8 +210,6 @@ namespace TehGM.EinherjiBot.Administration
             await response.ModifyAsync(props => props.Content = builder.ToString(), cancellationToken).ConfigureAwait(false);
         }
 
-
-
         // helper method for verifying a valid channel ID
         private async Task<SocketVoiceChannel> VerifyValidVoiceChannelAsync(Group matchGroup, IGuild guild, ISocketMessageChannel responseChannel, CancellationToken cancellationToken)
         {
@@ -249,5 +274,69 @@ namespace TehGM.EinherjiBot.Administration
 
         private static bool CanUserConnect(IVoiceChannel channel, IGuildUser user)
             => user.GetPermissions(channel).Has(ChannelPermission.ViewChannel | ChannelPermission.Connect);
+
+        private async Task<VoiceChannelMatch> MatchChannelAsync(SocketCommandContext context, Match match, int groupIndex, CancellationToken cancellationToken)
+        {
+            SocketVoiceChannel targetChannel = null;
+            SocketGuildUser user = context.Guild.GetUser(context.User.Id);
+            if (match.Groups.Count >= groupIndex + 1 && match.Groups[groupIndex].Success)
+                targetChannel = await this.VerifyValidVoiceChannelAsync(match.Groups[groupIndex], context.Guild, context.Channel, cancellationToken).ConfigureAwait(false);
+            else
+                targetChannel = user.VoiceChannel;
+            if (targetChannel == null)
+            {
+                await context.ReplyAsync($"{this._einherjiOptions.CurrentValue.FailureSymbol} You need to either provide a voice channel ID, or be in a voice channel currently. Also ensure I have access to that voice channel.", cancellationToken).ConfigureAwait(false);
+            }
+            return new VoiceChannelMatch(targetChannel, user);
+        }
+
+        private class VoiceChannelMatch
+        {
+            public SocketVoiceChannel Channel { get; }
+            public bool IsSuccess => this.Channel != null;
+            public SocketGuildUser User { get; }
+
+            public VoiceChannelMatch(SocketVoiceChannel channel, SocketGuildUser user)
+            {
+                this.Channel = channel;
+                this.User = user;
+            }
+        }
+
+        private class VoiceChannelAction
+        {
+            public string ModeWord { get; init; }
+            public string ActionCompletedWord { get; init; }
+            public string ActionFailedWord { get; init; }
+            public Action<GuildUserProperties> Action { get; }
+            public ChannelPermission RequiredPermission { get; }
+
+            public string PermissionDisplayName
+            {
+                get
+                {
+                    switch (this.RequiredPermission)
+                    {
+                        case ChannelPermission.DeafenMembers:
+                            return "Deafen Members";
+                        case ChannelPermission.MoveMembers:
+                            return "Move Members";
+                        case ChannelPermission.MuteMembers:
+                            return "Mute Members";
+                        default:
+                            return this.RequiredPermission.ToString();
+                    }
+                }
+            }
+
+            public VoiceChannelAction(Action<GuildUserProperties> action, ChannelPermission requiredPermission, string modeWord, string completeWord, string failWord)
+            {
+                this.Action = action;
+                this.RequiredPermission = requiredPermission;
+                this.ModeWord = modeWord;
+                this.ActionCompletedWord = completeWord;
+                this.ActionFailedWord = failWord;
+            }
+        }
     }
 }
