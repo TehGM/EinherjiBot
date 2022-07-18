0162060fca938ebf621095ef52ea928f2d4521ef
TehGM <TehGM@users.noreply.github.com>
2022-07-19 00:43:39 +0100

    Provide shortcut to cancellation token in module base

--- EinherjiBot/DiscordClient/EinherjiInteractionModule.cs
+++ EinherjiBot/DiscordClient/EinherjiInteractionModule.cs
@@ -19,6 +19,8 @@ namespace TehGM.EinherjiBot
 
     public class EinherjiInteractionModule : EinherjiInteractionModule<EinherjiInteractionContext>
     {
+        protected CancellationToken CancellationToken => base.Context.CancellationToken;
+
         public Task RespondAsync(string text, Embed embed)
             => base.RespondAsync(text, embed, base.Context.CancellationToken);
         public Task RespondAsync(string text)
