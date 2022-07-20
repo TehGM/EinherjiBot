23ce36ae788da743c2aaf015fa9973d3d70d1fb9
TehGM <TehGM@users.noreply.github.com>
2022-07-20 21:30:06 +0100

    Add discriminators to audits

--- EinherjiBot/Security/Auditing/AuditingServiceCollectionExtensions.cs
+++ EinherjiBot/Security/Auditing/AuditingServiceCollectionExtensions.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using TehGM.EinherjiBot.Auditing;
 using TehGM.EinherjiBot.Auditing.Services;
 
@@ -14,7 +18,30 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddMongoDB();
             services.TryAddSingleton(typeof(IAuditStore<>), typeof(MongoAuditStore<>));
 
+            MapAuditTypes();
+
             return services;
+        }
+
+        private static void MapAuditTypes()
+        {
+            IEnumerable<Type> auditTypes = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => typeof(BotAuditEntry).IsAssignableFrom(t)
+                    && !t.IsAbstract
+                    && !Attribute.IsDefined(t, typeof(CompilerGeneratedAttribute)));
+            foreach (Type type in auditTypes)
+            {
+                if (BsonClassMap.IsClassMapRegistered(type))
+                    continue;
+
+                string discriminator = type.GetCustomAttribute<BsonDiscriminatorAttribute>()?.Discriminator
+                    ?? type.Name;
+                BsonClassMap map = new BsonClassMap(type);
+                map.SetDiscriminatorIsRequired(true);
+                map.SetDiscriminator(discriminator);
+                BsonClassMap.RegisterClassMap(map);
+            }
         }
     }
 }
