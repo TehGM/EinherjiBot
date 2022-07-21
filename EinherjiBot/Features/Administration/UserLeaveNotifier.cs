4a181d107d7a63e99701a903ed3f346ab281c729
TehGM <TehGM@users.noreply.github.com>
2022-07-21 20:31:52 +0100

    Only post leave notification if bot has permission

--- EinherjiBot/Features/Administration/UserLeaveNotifier.cs
+++ EinherjiBot/Features/Administration/UserLeaveNotifier.cs
@@ -16,15 +16,27 @@ namespace TehGM.EinherjiBot.Features.Administration
             this._client.UserLeft += this.OnUserLeftAsync;
         }
 
-        protected Task OnUserLeftAsync(SocketGuild guild, SocketUser user)
+        protected async Task OnUserLeftAsync(SocketGuild guild, SocketUser user)
         {
             this._log.LogDebug("User {User} ({UserID}) left guild {Guild} ({GuildID})", user.GetUsernameWithDiscriminator(), user.Id, guild.Name, guild.Id);
             if (guild.SystemChannel == null)
-                return Task.CompletedTask;
+            {
+                this._log.LogDebug("{Guild} ({GuildID}) has no system channel, skipping notification", guild.Name, guild.Id);
+                return;
+            }
+
+            IGuildUser currentUser = await guild.GetGuildUserAsync(this._client.CurrentUser.Id, base.CancellationToken).ConfigureAwait(false);
+            ChannelPermissions permissions = currentUser.GetPermissions(guild.SystemChannel);
+            if (!permissions.SendMessages)
+            {
+                this._log.LogDebug("No permissions to post in {Guild}'s ({GuildID}) system channel, skipping notification", guild.Name, guild.Id);
+                return;
+            }
+
             EmbedBuilder embed = new EmbedBuilder()
                 .WithDescription($"**{user.Mention}** *(`{user.Username}#{user.Discriminator}`)* **has left.**")
                 .WithColor((Color)System.Drawing.Color.Cyan);
-            return guild.SystemChannel.SendMessageAsync(null, false, embed.Build(), base.CancellationToken);
+            await guild.SystemChannel.SendMessageAsync(null, false, embed.Build(), base.CancellationToken);
         }
 
         public override void Dispose()
