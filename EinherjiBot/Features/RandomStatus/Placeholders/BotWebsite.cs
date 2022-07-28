b18a023ab03482f34919bb83f04c7fcdd6380165
TehGM <TehGM@users.noreply.github.com>
2022-07-28 11:19:02 +0100

    Added bot website placeholder

---
+++ EinherjiBot/Features/RandomStatus/Placeholders/BotWebsite.cs
@@ -0,0 +1,9 @@
+﻿namespace TehGM.EinherjiBot.RandomStatus.Placeholders
+{
+    [StatusPlaceholder("{{BotWebsite}}")]
+    public class BotWebsite : IStatusPlaceholder
+    {
+        public Task<string> GetReplacementAsync(Match placeholder, CancellationToken cancellationToken = default)
+            => Task.FromResult(EinherjiInfo.WebsiteURL);
+    }
+}
