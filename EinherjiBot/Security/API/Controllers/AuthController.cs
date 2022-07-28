7c0dddbea2db1efb3b5305644c518726cd1a1b07
TehGM <TehGM@users.noreply.github.com>
2022-07-28 13:45:56 +0100

    Move controller to its own folder

--- EinherjiBot/Security/API/Controllers/AuthController.cs
+++ EinherjiBot/Security/API/Controllers/AuthController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TehGM.EinherjiBot.Security.API
+namespace TehGM.EinherjiBot.Security.API.Controllers
 {
     [Route("api/auth/token")]
     [ApiController]
