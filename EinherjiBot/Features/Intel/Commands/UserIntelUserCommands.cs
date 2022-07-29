a8a8ad3c3dcdc99bc2042a4880f7891e0dd6627a
TehGM <TehGM@users.noreply.github.com>
2022-07-29 14:07:37 +0100

    Added user intel right click command

---
+++ EinherjiBot/Features/Intel/Commands/UserIntelUserCommands.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Interactions;
+using TehGM.EinherjiBot.Auditing;
+using TehGM.EinherjiBot.Auditing.Intel;
+
+namespace TehGM.EinherjiBot.Intel.Commands
+{
+    public class UserIntelUserCommands : EinherjiInteractionModule
+    {
+        private readonly IIntelEmbedBuilder _embed;
+        private readonly IAuditStore<UserIntelAuditEntry> _audit;
+
+        public UserIntelUserCommands(IIntelEmbedBuilder embedBuilder, IAuditStore<UserIntelAuditEntry> audit)
+        {
+            this._embed = embedBuilder;
+            this._audit = audit;
+        }
+
+        [UserCommand("Get Intel")]
+        public async Task CmdIntelUserAsync(IUser user)
+        {
+            Embed embed = await this._embed.BuildUserEmbedAsync(user, base.Context.Guild, base.CancellationToken).ConfigureAwait(false);
+            await this._audit.AddAuditAsync(new UserIntelAuditEntry(base.Context.User.Id, user.Id, base.Context.Interaction.CreatedAt.UtcDateTime));
+            await base.RespondAsync(null, embed).ConfigureAwait(false);
+        }
+    }
+}
