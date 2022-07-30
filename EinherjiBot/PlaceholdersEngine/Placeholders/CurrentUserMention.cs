7416733c660b4b6010da9c03dd85f88379d957f7
TehGM <TehGM@users.noreply.github.com>
2022-07-31 00:27:21 +0100

    Add current user placeholders

---
+++ EinherjiBot/PlaceholdersEngine/Placeholders/CurrentUserMention.cs
@@ -0,0 +1,16 @@
+﻿namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
+{
+    [Placeholder("{{CurrentUserMention}}")]
+    public class CurrentUserMention : IPlaceholder
+    {
+        private readonly IDiscordAuthContext _auth;
+
+        public CurrentUserMention(IDiscordAuthContext authContext)
+        {
+            this._auth = authContext;
+        }
+
+        public Task<string> GetReplacementAsync(Match placeholder, CancellationToken cancellationToken = default)
+            => Task.FromResult(this._auth.DiscordUser.Mention);
+    }
+}
---
+++ EinherjiBot/PlaceholdersEngine/Placeholders/CurrentUserName.cs
@@ -0,0 +1,40 @@
+﻿namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
+{
+    [Placeholder($"{{{{CurrentUserName(?::({_modeNickname}|{_modeUsername}|{_modeUsernameWithDiscriminator}))?(?::({_modeNickname}|{_modeUsername}|{_modeUsernameWithDiscriminator}))?}}}}")]
+    public class CurrentUserName : IPlaceholder
+    {
+        private const string _modeNickname = "Nickname";
+        private const string _modeUsername = "Username";
+        private const string _modeUsernameWithDiscriminator = "UsernameWithDiscriminator";
+        private const string _defaultMode = _modeNickname;
+        private const string _defaultFallbackMode = _modeUsername;
+
+        private readonly IDiscordAuthContext _auth;
+
+        public CurrentUserName(IDiscordAuthContext authContext)
+        {
+            this._auth = authContext;
+        }
+
+        public Task<string> GetReplacementAsync(Match placeholder, CancellationToken cancellationToken = default)
+        {
+            string mode = _defaultMode;
+            if (placeholder.Groups[1].Success)
+                mode = placeholder.Groups[1].Value;
+
+            if (mode == _modeNickname)
+            {
+                if (!string.IsNullOrWhiteSpace(this._auth.DiscordGuildUser?.Nickname))
+                    return Task.FromResult(this._auth.DiscordGuildUser.Nickname);
+                mode = _defaultFallbackMode;
+                if (placeholder.Groups[2].Success)
+                    mode = placeholder.Groups[2].Value;
+            }
+
+            if (mode == _modeUsername)
+                return Task.FromResult(this._auth.DiscordUser.Username);
+            else
+                return Task.FromResult(this._auth.DiscordUser.GetUsernameWithDiscriminator());
+        }
+    }
+}
