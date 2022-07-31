5d0d4f5c969b7ea98e158ed1d22050c8f0597da5
TehGM <TehGM@users.noreply.github.com>
2022-07-31 01:55:41 +0100

    Add placeholder for current message content

---
+++ EinherjiBot/PlaceholdersEngine/Placeholders/CurrentMessageContent.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+
+namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
+{
+    // this placeholder will break when not in message context (like message trigger)
+    [Placeholder("{{CurrentMessageContent}}")]
+    public class CurrentMessageContent : IPlaceholder
+    {
+        private readonly IMessage _message;
+
+        public CurrentMessageContent(IMessage message)
+        {
+            this._message = message;
+        }
+
+        public Task<string> GetReplacementAsync(Match placeholder, CancellationToken cancellationToken = default)
+            => Task.FromResult(this._message.Content);
+    }
+}
