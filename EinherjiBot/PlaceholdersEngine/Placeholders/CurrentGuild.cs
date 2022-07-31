a3c018b61ed42074b4497b2326bd52298e0d4bb6
TehGM <TehGM@users.noreply.github.com>
2022-07-31 02:57:05 +0100

    Added current guild placeholder

---
+++ EinherjiBot/PlaceholdersEngine/Placeholders/CurrentGuild.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+
+namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
+{
+    // this placeholder will break when not in message context (like message trigger)
+    [Placeholder("{{CurrentGuild}}")]
+    public class CurrentGuild : IPlaceholder
+    {
+        private readonly IMessage _message;
+
+        public CurrentGuild(IMessage message)
+        {
+            this._message = message;
+        }
+
+        public Task<string> GetReplacementAsync(Match placeholder, CancellationToken cancellationToken = default)
+        {
+            if (this._message.Channel is not IGuildChannel channel)
+                throw new InvalidOperationException($"{nameof(CurrentGuild)} placeholder can only be used for guild messages");
+            return Task.FromResult(channel.Guild.Name);
+        }
+    }
+}
