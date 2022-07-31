17d3028cdc00838dcfd70c78f14a32e5ed0c3265
TehGM <TehGM@users.noreply.github.com>
2022-07-31 03:08:14 +0100

    Disable guild name placeholder cache

--- EinherjiBot/PlaceholdersEngine/Placeholders/Guild.cs
+++ EinherjiBot/PlaceholdersEngine/Placeholders/Guild.cs
@@ -2,23 +2,18 @@
 
 namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
 {
-    [Placeholder($"{{{{GuildName:(\\d{{1,20}})}}}}")]
-    internal class GuildName : IPlaceholder
+    [Placeholder($"{{{{Guild:(\\d{{1,20}})}}}}")]
+    internal class Guild : IPlaceholder
     {
         private readonly IDiscordClient _client;
 
-        private string _name;
-
-        public GuildName(IDiscordClient client)
+        public Guild(IDiscordClient client)
         {
             this._client = client;
         }
 
         public async Task<string> GetReplacementAsync(Match placeholder, CancellationToken cancellationToken = default)
         {
-            if (!string.IsNullOrEmpty(this._name))
-                return this._name;
-
             if (!placeholder.Groups[1].Success)
                 throw new ArgumentException($"Placeholder requires a valid channel ID to be provided");
             if (!ulong.TryParse(placeholder.Groups[1].Value, out ulong id))
@@ -28,8 +23,7 @@ namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
             if (guild == null)
                 throw new InvalidOperationException($"Discord guild with ID {id} not found");
 
-            this._name = guild.Name;
-            return this._name;
+            return guild.Name;
         }
     }
 }
