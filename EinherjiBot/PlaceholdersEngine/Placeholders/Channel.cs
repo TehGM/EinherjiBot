241a149ca673888d9d6fc8246b877b92c069f4d7
TehGM <TehGM@users.noreply.github.com>
2022-07-31 03:07:47 +0100

    Updated channel placeholder to support mentions

--- EinherjiBot/PlaceholdersEngine/Placeholders/Channel.cs
+++ EinherjiBot/PlaceholdersEngine/Placeholders/Channel.cs
@@ -2,23 +2,22 @@
 
 namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
 {
-    [Placeholder($"{{{{ChannelName:(\\d{{1,20}})}}}}")]
-    internal class ChannelName : IPlaceholder
+    [Placeholder($"{{{{Channel:(\\d{{1,20}})(?::({_modeMention}|{_modeName}))?}}}}")]
+    internal class Channel : IPlaceholder
     {
+        private const string _modeMention = "Mention";
+        private const string _modeName = "Name";
+        private const string _defaultMode = _modeMention;
+
         private readonly IDiscordClient _client;
 
-        private string _name;
-
-        public ChannelName(IDiscordClient client)
+        public Channel(IDiscordClient client)
         {
             this._client = client;
         }
 
         public async Task<string> GetReplacementAsync(Match placeholder, CancellationToken cancellationToken = default)
         {
-            if (!string.IsNullOrEmpty(this._name))
-                return this._name;
-
             if (!placeholder.Groups[1].Success)
                 throw new ArgumentException($"Placeholder requires a valid channel ID to be provided");
             if (!ulong.TryParse(placeholder.Groups[1].Value, out ulong id))
@@ -28,8 +27,15 @@ namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
             if (channel == null)
                 throw new InvalidOperationException($"Discord channel with ID {id} not found");
 
-            this._name = channel.Name;
-            return this._name;
+
+            string mode = _defaultMode;
+            if (placeholder.Groups[1].Success)
+                mode = placeholder.Groups[1].Value;
+
+            if (mode == _modeMention)
+                return MentionUtils.MentionChannel(channel.Id);
+            else
+                return channel.Name;
         }
     }
 }
