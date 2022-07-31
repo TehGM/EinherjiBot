44b33f5ce01879a21c1febf67f9085b724d70fa3
TehGM <TehGM@users.noreply.github.com>
2022-07-31 12:58:49 +0100

    Added some general purpose auth policies

---
+++ EinherjiBot/Security/Authorization/Policies/Authorize.cs
@@ -0,0 +1,26 @@
+﻿namespace TehGM.EinherjiBot.Security.Authorization.Policies
+{
+    public class Authorize : IDiscordAuthorizationPolicy
+    {
+        protected IDiscordAuthContext Auth { get; }
+
+        public Authorize(IDiscordAuthContext auth)
+        {
+            this.Auth = auth;
+        }
+
+        public virtual Task<DiscordAuthorizationResult> EvaluateAsync(CancellationToken cancellationToken = default)
+        {
+            if (!this.Auth.IsLoggedIn())
+                return Task.FromResult(DiscordAuthorizationResult.Fail("Not logged in."));
+            if (this.Auth.IsBanned)
+                return Task.FromResult(DiscordAuthorizationResult.Fail($"You're banned in {EinherjiInfo.Name} system."));
+            return Task.FromResult(DiscordAuthorizationResult.Success);
+        }
+    }
+
+    public class AuthorizeAttribute : DiscordAuthorizationAttribute
+    {
+        public AuthorizeAttribute() : base(typeof(Authorize)) { }
+    }
+}
---
+++ EinherjiBot/Security/Authorization/Policies/AuthorizeAdmin.cs
@@ -0,0 +1,24 @@
+﻿namespace TehGM.EinherjiBot.Security.Authorization.Policies
+{
+    public class AuthorizeAdmin : Authorize, IDiscordAuthorizationPolicy
+    {
+        public AuthorizeAdmin(IDiscordAuthContext auth) : base(auth) { }
+
+        public override async Task<DiscordAuthorizationResult> EvaluateAsync(CancellationToken cancellationToken = default)
+        {
+            DiscordAuthorizationResult result = await base.EvaluateAsync(cancellationToken).ConfigureAwait(false);
+            if (!result.Succeeded)
+                return result;
+
+            if (!base.Auth.IsAdmin())
+                return DiscordAuthorizationResult.Fail($"You're not a {EinherjiInfo.Name} admin.");
+
+            return DiscordAuthorizationResult.Success;
+        }
+    }
+
+    public class AuthorizeAdminAttribute : DiscordAuthorizationAttribute
+    {
+        public AuthorizeAdminAttribute() : base(typeof(AuthorizeAdmin)) { }
+    }
+}
