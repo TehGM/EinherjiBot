fdee7cadae550ac4fc8a91c6487310e72ca8fa48
TehGM <TehGM@users.noreply.github.com>
2022-07-31 03:11:47 +0100

    Support modes for user placeholder

--- EinherjiBot/PlaceholdersEngine/Placeholders/User.cs
+++ EinherjiBot/PlaceholdersEngine/Placeholders/User.cs
@@ -2,23 +2,23 @@
 
 namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
 {
-    [Placeholder($"{{{{UserName:(\\d{{1,20}})}}}}")]
-    internal class UserName : IPlaceholder
+    [Placeholder($"{{{{User:(\\d{{1,20}})}}}}(?::({_modeMention}|{_modeUsername}|{_modeUsernameWithDiscriminator}))?")]
+    internal class User : IPlaceholder
     {
+        private const string _modeMention = "Mention";
+        private const string _modeUsername = "Username";
+        private const string _modeUsernameWithDiscriminator = "UsernameWithDiscriminator";
+        private const string _defaultMode = _modeUsername;
+
         private readonly IDiscordClient _client;
 
-        private string _name;
-
-        public UserName(IDiscordClient client)
+        public User(IDiscordClient client)
         {
             this._client = client;
         }
 
         public async Task<string> GetReplacementAsync(Match placeholder, CancellationToken cancellationToken = default)
         {
-            if (!string.IsNullOrEmpty(this._name))
-                return this._name;
-
             if (!placeholder.Groups[1].Success)
                 throw new ArgumentException($"Placeholder requires a valid user ID to be provided");
             if (!ulong.TryParse(placeholder.Groups[1].Value, out ulong id))
@@ -27,8 +27,18 @@ namespace TehGM.EinherjiBot.PlaceholdersEngine.Placeholders
             IUser user = await this._client.GetUserAsync(id, CacheMode.AllowDownload, cancellationToken.ToRequestOptions()).ConfigureAwait(false);
             if (user == null)
                 throw new InvalidOperationException($"Discord user with ID {id} not found");
-            this._name = user.Username;
-            return this._name;
+
+            string mode = _defaultMode;
+            if (placeholder.Groups[1].Success)
+                mode = placeholder.Groups[1].Value;
+
+            if (mode == _modeMention)
+                return user.Mention;
+
+            if (mode == _modeUsername)
+                return user.Username;
+            else
+                return user.GetUsernameWithDiscriminator();
         }
     }
 }
