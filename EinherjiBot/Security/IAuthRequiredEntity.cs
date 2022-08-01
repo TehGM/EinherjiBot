329a5efc1b5cd6ff12b6c2b66a04437b5c076264
TehGM <TehGM@users.noreply.github.com>
2022-08-01 10:07:28 +0100

    Remove obsolete interface

--- EinherjiBot/Security/IAuthRequiredEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TehGM.EinherjiBot.Security
-{
-    public interface IAuthRequiredEntity
-    {
-        // TODO: this is quite naive right now. Probably want to transition to policies or something.
-        bool CanAccess(IDiscordAuthContext context);
-        bool CanEdit(IDiscordAuthContext context);
-    }
-}
