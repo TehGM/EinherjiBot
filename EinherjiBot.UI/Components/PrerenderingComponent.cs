ada96e56d31d73f77d33c795b5074b74461b87eb
TehGM <TehGM@users.noreply.github.com>
2022-08-01 18:45:47 +0100

    Persist prerendered state

---
+++ EinherjiBot.UI/Components/PrerenderingComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TehGM.EinherjiBot.UI
+{
+    public abstract class PrerenderingComponent : ComponentBase, IDisposable
+    {
+        [Inject]
+        protected PersistentComponentState PrerenderingState { get; init; }
+        private PersistingComponentStateSubscription _prerenderingSubscription;
+
+        protected override Task OnInitializedAsync()
+        {
+            this._prerenderingSubscription = this.PrerenderingState.RegisterOnPersisting(this.PersistAsync);
+            return Task.CompletedTask;
+        }
+
+        protected abstract Task PersistAsync();
+
+        public virtual void Dispose()
+        {
+            try { this._prerenderingSubscription.Dispose(); } catch { }
+        }
+    }
+}
