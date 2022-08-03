add9c4ddd6b37ca32ca5d4957d6bdcb7a186e124
TehGM <TehGM@users.noreply.github.com>
2022-08-03 11:16:24 +0100

    Create wrapper components for location/env conditional rendering

---
+++ EinherjiBot.UI/Components/Rendering/RenderingEnvironmentOnly.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace TehGM.EinherjiBot.UI.Components.Rendering
+{
+    public class RenderingEnvironmentOnly : ComponentBase
+    {
+        [Inject]
+        private IRenderLocation RenderLocation { get; init; }
+
+        [Parameter]
+        public string Environment { get; set; }
+
+        [Parameter, EditorRequired]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            if (!this.RenderLocation.EnvironmentName.Equals(this.Environment, StringComparison.OrdinalIgnoreCase))
+                return;
+
+            builder.AddContent(1, this.ChildContent);
+        }
+    }
+
+    public class DevelopmentOnly : RenderingEnvironmentOnly
+    {
+        public DevelopmentOnly()
+        {
+            base.Environment = "Development";
+        }
+    }
+
+    public class ProductionOnly : RenderingEnvironmentOnly
+    {
+        public ProductionOnly()
+        {
+            base.Environment = "Production";
+        }
+    }
+}
---
+++ EinherjiBot.UI/Components/Rendering/RenderingLocationOnly.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace TehGM.EinherjiBot.UI.Components.Rendering
+{
+    public class RenderingLocationOnly : ComponentBase
+    {
+        [Inject]
+        private IRenderLocation RenderLocation { get; init; }
+
+        [Parameter]
+        public RenderLocation Location { get; set; }
+
+        [Parameter, EditorRequired]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            if (this.RenderLocation.RenderLocation != this.Location)
+                return;
+
+            builder.AddContent(1, this.ChildContent);
+        }
+    }
+
+    public class ClientOnly : RenderingLocationOnly
+    {
+        public ClientOnly()
+        {
+            base.Location = RenderLocation.Client;
+        }
+    }
+
+    public class ServerOnly : RenderingLocationOnly
+    {
+        public ServerOnly()
+        {
+            base.Location = RenderLocation.Server;
+        }
+    }
+}
