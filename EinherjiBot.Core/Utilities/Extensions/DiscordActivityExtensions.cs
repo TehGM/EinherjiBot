5fa37137a73ff4a470d50d455cfe71a253d711eb
TehGM <TehGM@users.noreply.github.com>
2022-08-04 18:51:19 +0100

    Make preview look more discord-like

---
+++ EinherjiBot.Core/Utilities/Extensions/DiscordActivityExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+
+namespace TehGM.EinherjiBot
+{
+    public static class DiscordActivityExtensions
+    {
+        public static string ToDisplayString(this ActivityType activity)
+        {
+            switch (activity)
+            {
+                case ActivityType.Playing:
+                    return "Playing";
+                case ActivityType.Streaming:
+                    return "Streaming";
+                case ActivityType.Watching:
+                    return "Watching";
+                case ActivityType.Listening:
+                    return "Listening to";
+                case ActivityType.CustomStatus:
+                    return string.Empty;
+                default:
+                    return activity.ToString();
+            }
+        }
+    }
+}
