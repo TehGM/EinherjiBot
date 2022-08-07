6c9c688a128b9bfec4a6e7a14cb01d409d95b0d8
TehGM <TehGM@users.noreply.github.com>
2022-08-08 00:08:22 +0100

    Add guild name to the role preview

--- EinherjiBot.Core/API/RoleInfoResponse.cs
+++ EinherjiBot.Core/API/RoleInfoResponse.cs
@@ -10,16 +10,22 @@ namespace TehGM.EinherjiBot.API
         public ulong ID { get; init; }
         [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("guild")]
+        public ulong GuildID { get; init; }
+        [JsonProperty("guildName")]
+        public string GuildName { get; init; }
         [JsonProperty("color")]
         public uint Color { get; init; }
 
         [JsonConstructor]
         private RoleInfoResponse() { }
 
-        public RoleInfoResponse(ulong id, string name, uint color = 0)
+        public RoleInfoResponse(ulong id, string name, ulong guildID, string guildName, uint color = 0)
         {
             this.ID = id;
             this.Name = name;
+            this.GuildID = guildID;
+            this.GuildName = guildName;
             this.Color = color;
         }
 
--- EinherjiBot/API/Services/ApiRoleInfoService.cs
+++ EinherjiBot/API/Services/ApiRoleInfoService.cs
@@ -24,7 +24,7 @@ namespace TehGM.EinherjiBot.API.Services
                 if (role == null)
                     continue;
 
-                return new RoleInfoResponse(role.Id, role.Name, role.Color);
+                return new RoleInfoResponse(role.Id, role.Name, guild.Id, guild.Name, role.Color);
             }
             return null;
         }
