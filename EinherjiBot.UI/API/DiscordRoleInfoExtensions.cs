2f1320618e1746a8469086ae62684774cb5e86ae
TehGM <TehGM@users.noreply.github.com>
2022-08-09 11:39:07 +0100

    Shared way to get theme-aware role color

---
+++ EinherjiBot.UI/API/DiscordRoleInfoExtensions.cs
@@ -0,0 +1,10 @@
+﻿using TehGM.EinherjiBot.API;
+
+namespace TehGM.EinherjiBot.UI.API
+{
+    public static class DiscordRoleInfoExtensions
+    {
+        public static string GetHtmlColorOrDefault(this RoleInfoResponse response, string defaultValue = "var(--mud-palette-text-primary)")
+            => response.GetHtmlColor() ?? defaultValue;
+    }
+}
