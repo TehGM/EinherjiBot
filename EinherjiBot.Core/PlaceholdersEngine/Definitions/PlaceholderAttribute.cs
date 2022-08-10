273998585e8f05ea1606701357e10afdfd1785a0
TehGM <TehGM@users.noreply.github.com>
2022-08-10 20:23:45 +0100

    Added regex builder to placeholder attribute

--- EinherjiBot.Core/PlaceholdersEngine/Definitions/PlaceholderAttribute.cs
+++ EinherjiBot.Core/PlaceholdersEngine/Definitions/PlaceholderAttribute.cs
@@ -1,10 +1,16 @@
-﻿namespace TehGM.EinherjiBot.PlaceholdersEngine
+﻿using System.Text.RegularExpressions;
+
+namespace TehGM.EinherjiBot.PlaceholdersEngine
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
     public class PlaceholderAttribute : Attribute
     {
         public string Identifier { get; }
         public PlaceholderUsage AllowedContext { get; init; }
+
+        public Regex MatchingRegex { get; }
+
+        private const string _regexParamsCharset = $"A-Za-z0-9 {PlaceholderSymbol.ParameterSplitter}{PlaceholderSymbol.KeyValueSplitter}";
 
         public PlaceholderAttribute(string identifier, PlaceholderUsage allowedContext)
         {
@@ -13,6 +19,9 @@
 
             this.Identifier = identifier;
             this.AllowedContext = allowedContext;
+
+            this.MatchingRegex = new Regex($"{PlaceholderSymbol.OpenTag}{this.Identifier}(?:[{Regex.Escape(_regexParamsCharset)}]*?)?{PlaceholderSymbol.CloseTag}",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Singleline);
         }
     }
 }
