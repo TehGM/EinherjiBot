f6eb3f523b294c2fb558c9f1a94c6215e7e742bf
TehGM <TehGM@users.noreply.github.com>
2022-08-12 23:22:31 +0100

    Fix wrong ID type on channel placeholder

--- EinherjiBot.Core/PlaceholdersEngine/Placeholders/Channel.cs
+++ EinherjiBot.Core/PlaceholdersEngine/Placeholders/Channel.cs
@@ -3,7 +3,7 @@
     [Placeholder("Channel", PlaceholderUsage.Any)]
     public class ChannelPlaceholder
     {
-        [PlaceholderProperty("ID", IsRequired = true, IDType = IDType.User)]
+        [PlaceholderProperty("ID", IsRequired = true, IDType = IDType.Channel)]
         public ulong ChannelID { get; init; }
         [PlaceholderProperty("Mode")]
         public ChannelDisplayMode DisplayMode { get; init; } = ChannelDisplayMode.Mention;
