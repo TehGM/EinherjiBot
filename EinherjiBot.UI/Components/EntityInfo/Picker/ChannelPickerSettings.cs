90585cac8a8799e42f6664705ae2fb0742b22cec
TehGM <TehGM@users.noreply.github.com>
2022-08-12 18:39:11 +0100

    Add channel picker

---
+++ EinherjiBot.UI/Components/EntityInfo/Picker/ChannelPickerSettings.cs
@@ -0,0 +1,12 @@
+﻿using Discord;
+
+namespace TehGM.EinherjiBot.UI.Components.EntityInfo.Picker
+{
+    public class ChannelPickerSettings : EntityPickerSettings
+    {
+        /// <summary>If set, only these channel types will be included in the picker.</summary>
+        public IEnumerable<ChannelType> ShownChannelTypes { get; init; }
+        /// <summary>If set, only these channel types will be pickable. Others will still be displayed for hierarchy purposes.</summary>
+        public IEnumerable<ChannelType> PickableChannelTypes { get; init; }
+    }
+}
