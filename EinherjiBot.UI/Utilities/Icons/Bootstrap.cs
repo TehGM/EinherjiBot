aa46c1d4bf7059bb9a9a8121ecbed45e7ddb502d
TehGM <TehGM@users.noreply.github.com>
2022-08-23 19:27:52 +0100

    Improve filtering of guilds section

---
+++ EinherjiBot.UI/Utilities/Icons/Bootstrap.cs
@@ -0,0 +1,19 @@
+﻿namespace TehGM.EinherjiBot.UI
+{
+    // use similar nested class as MudBlazor icons, purely for consistency
+    // https://icons.getbootstrap.com/
+    public static class BootstrapIcons
+    {
+        public static class Outlined
+        {
+            public const string ShieldCheck = "bi bi-shield-check";
+            public const string ShieldSlash = "bi bi-shield-slash";
+        }
+
+        public static class Filled
+        {
+            public const string ShieldCheck = "bi bi-shield-fill-check";
+            public const string ShieldSlash = "bi bi-shield-slash-fill";
+        }
+    }
+}
