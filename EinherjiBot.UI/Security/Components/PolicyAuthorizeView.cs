27092400cb4cb0a22412a427833ae7b0b3b4f26e
TehGM <TehGM@users.noreply.github.com>
2022-08-25 13:24:33 +0100

    Allow using resource with PolicyAuthorizeView

--- EinherjiBot.UI/Security/Components/PolicyAuthorizeView.cs
+++ EinherjiBot.UI/Security/Components/PolicyAuthorizeView.cs
@@ -5,26 +5,65 @@ using TehGM.EinherjiBot.Security;
 
 namespace TehGM.EinherjiBot.UI.Security.Components
 {
-    public class PolicyAuthorizeView : AuthorizeView
+    public class PolicyAuthorizeView : PolicyAuthorizeViewCore
     {
-        private AuthenticationState _currentAuthenticationState;
-        private bool _isAuthorized;
+        [Parameter]
+        public Func<Task<BotAuthorizationResult>> AuthorizationDelegate { get; set; }
 
+        protected override async Task<BotAuthorizationResult> PerformAuthorizationAsync(IEnumerable<Type> policies)
+        {
+            BotAuthorizationResult result = await this.AuthService.AuthorizeAsync(this.AllPolicies);
+
+            if (result.Succeeded && this.AuthorizationDelegate != null)
+                result = await this.AuthorizationDelegate.Invoke();
+
+            return result;
+        }
+    }
+
+    public class ResourceAuthorizeView<TResource> : PolicyAuthorizeViewCore
+    {
+        [Parameter]
+        public Func<TResource, Task<BotAuthorizationResult>> AuthorizationDelegate { get; set; }
+
+        protected override async Task<BotAuthorizationResult> PerformAuthorizationAsync(IEnumerable<Type> policies)
+        {
+            BotAuthorizationResult result = await this.AuthService.AuthorizeAsync((TResource)this.Resource, this.AllPolicies);
+
+            if (result.Succeeded && this.AuthorizationDelegate != null)
+                result = await this.AuthorizationDelegate.Invoke((TResource)this.Resource);
+
+            return result;
+        }
+    }
+
+    public abstract class PolicyAuthorizeViewCore : AuthorizeView
+    {
         [Parameter]
         public Type PolicyType { get; set; }
         [Parameter]
         public IEnumerable<Type> PolicyTypes { get; set; }
+        [Parameter]
+        public EventCallback AuthorizationSucceeded { get; set; }
+        [Parameter]
+        public EventCallback<BotAuthorizationResult> AuthorizationFailed { get; set; }
 
         // for route view
         [Parameter]
-        public RouteData RouteData { get; set; } = default!;
+        public RouteData RouteData { get; set; }
 
         [Inject]
-        private IBotAuthorizationService AuthService { get; set; }
+        protected IRenderLocation RenderLocation { get; private set; }
+        [Inject]
+        protected IBotAuthorizationService AuthService { get; private set; }
         [CascadingParameter]
-        private Task<AuthenticationState> AuthenticationStateTask { get; set; }
+        protected Task<AuthenticationState> AuthenticationStateTask { get; private set; }
 
-        private IEnumerable<Type> _allPolicies;
+        protected AuthenticationState CurrentAuthenticationState { get; private set; }
+        protected BotAuthorizationResult AuthorizationResult { get; set; }
+        protected IEnumerable<Type> AllPolicies { get; private set; }
+
+        protected bool IsAuthorized => this.AuthorizationResult?.Succeeded == true;
 
         private IEnumerable<Type> GetPolicies()
         {
@@ -35,28 +74,38 @@ namespace TehGM.EinherjiBot.UI.Security.Components
                 .Where(p => p is not null);
         }
 
+        protected abstract Task<BotAuthorizationResult> PerformAuthorizationAsync(IEnumerable<Type> policies);
+
         protected override async Task OnParametersSetAsync()
         {
-            this._allPolicies = this.GetPolicies();
-            this._currentAuthenticationState = await this.AuthenticationStateTask.ConfigureAwait(false);
-            BotAuthorizationResult result = await this.AuthService.AuthorizeAsync(this._allPolicies).ConfigureAwait(false);
-            this._isAuthorized = result.Succeeded;
+            this.AllPolicies = this.GetPolicies();
+            this.CurrentAuthenticationState = await this.AuthenticationStateTask.ConfigureAwait(false);
+
+            if (this.RenderLocation.IsServer)
+                this.AuthorizationResult = BotAuthorizationResult.Fail("Pre-rendering does not support authorization");
+            else
+                this.AuthorizationResult = await this.PerformAuthorizationAsync(this.AllPolicies).ConfigureAwait(false);
+
+            if (this.IsAuthorized)
+                await this.AuthorizationSucceeded.InvokeAsync();
+            else
+                await this.AuthorizationFailed.InvokeAsync(this.AuthorizationResult);
         }
 
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
-            if (this._isAuthorized || this._allPolicies?.Any() != true)
+            if (this.IsAuthorized)
             {
                 RenderFragment<AuthenticationState> authorizedContent = base.Authorized ?? base.ChildContent;
-                builder.AddContent(1, authorizedContent?.Invoke(this._currentAuthenticationState));
+                builder.AddContent(1, authorizedContent?.Invoke(this.CurrentAuthenticationState));
             }
-            else if (this._currentAuthenticationState == null)
+            else if (this.CurrentAuthenticationState == null)
             {
                 builder.AddContent(0, base.Authorizing);
             }
             else
             {
-                builder.AddContent(2, base.NotAuthorized?.Invoke(this._currentAuthenticationState));
+                builder.AddContent(2, base.NotAuthorized?.Invoke(this.CurrentAuthenticationState));
             }
         }
     }
