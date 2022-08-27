7b688a3be887a92a651bad0fb8aaa88e283ad45f
TehGM <TehGM@users.noreply.github.com>
2022-08-27 19:25:12 +0100

    Add bad request API exception

---
+++ EinherjiBot.Core/API/Exceptions/BadRequestException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace TehGM.EinherjiBot.API
+{
+    public class BadRequestException : ApiException
+    {
+        public BadRequestException(string message, Exception innerException)
+            : base(message, innerException, HttpStatusCode.BadRequest) { }
+        public BadRequestException(string message)
+            : this(message, null) { }
+        public BadRequestException()
+            : this(null) { }
+    }
+}
